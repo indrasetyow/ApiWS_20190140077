f897b60f00530d04ede1457624ba48e2ea827aa7
indrasetyow <79885192+indrasetyow@users.noreply.github.com>
2022-01-06 11:35:58 +0700

    ApiWs

--- src/main/java/ti/umy/TugasAPI/controller/BookController.java
+++ src/main/java/ti/umy/TugasAPI/controller/BookController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import ti.umy.TugasAPI.entity.Daftarbuku;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/book")
 public class BookController {
     @Autowired
